33875dca4b2b00f3cf54f021aa0d38b0a259473f
patrick-vieira <vieira_patrick@live.com>
2015-05-26 11:21:07 -0300

    first

---
+++ projeto/projeto/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(projeto.Startup))]
+namespace projeto
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
